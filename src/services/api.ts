1c1b9b05ab30d0c0fe4fdc504e38e8d4d8af478e
Chhaiann <chhaiann@Chhaianns-MacBook-Pro.local>
2025-12-14 16:38:15 +0700

    update client url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://minishop-api.vercel.app/api',
     headers: {
         'Cache-Control': 'no-cache'
     }
